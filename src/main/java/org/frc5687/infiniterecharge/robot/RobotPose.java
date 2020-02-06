2c770a9065885d930b24d5659bc5ff5d9b892b4e
BenBernardCIS <benbernard@post.harvard.edu>
2020-02-05 19:30:53 -0500

    Updating pose

--- src/main/java/org/frc5687/infiniterecharge/robot/RobotPose.java
+++ src/main/java/org/frc5687/infiniterecharge/robot/RobotPose.java
@@ -1,4 +1,19 @@
 package org.frc5687.infiniterecharge.robot;
 
-public class RobotPose {
+import org.frc5687.infiniterecharge.robot.util.BasicPose;
+import org.frc5687.infiniterecharge.robot.util.Pose;
+
+public class RobotPose extends BasicPose {
+    private double _turretAngle;
+    private double _hoodAngle;
+
+    public RobotPose(double angle, double leftEncoder, double rightEncoder, double distance, double turretAngle, double hoodAngle) {
+        super(angle, leftEncoder, rightEncoder, distance);
+        _turretAngle = turretAngle;
+        _hoodAngle = hoodAngle;
+    }
+
+    public double getTurretAngle() { return _turretAngle; }
+
+    public double getHoodAngle() { return _hoodAngle; }
 }
