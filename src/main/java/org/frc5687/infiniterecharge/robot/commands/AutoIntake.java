3273cc38763d2add4021cd94434274917589c59c
BenBernardCIS <benbernard@post.harvard.edu>
2020-02-05 20:40:32 -0500

    Added auto-intake

---
+++ src/main/java/org/frc5687/infiniterecharge/robot/commands/AutoIntake.java
@@ -0,0 +1,35 @@
+package org.frc5687.infiniterecharge.robot.commands;
+
+import org.frc5687.infiniterecharge.robot.Constants;
+import org.frc5687.infiniterecharge.robot.subsystems.Intake;
+
+public class AutoIntake extends OutliersCommand {
+    private Intake _intake;
+
+    public AutoIntake(Intake intake) {
+        _intake = intake;
+        addRequirements(_intake);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        _intake.lowerIntake();
+    }
+
+    @Override
+    public void execute() {
+        _intake.setSpeed(Constants.Intake.INTAKE_SPEED);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        _intake.raiseIntake();
+    }
+}
