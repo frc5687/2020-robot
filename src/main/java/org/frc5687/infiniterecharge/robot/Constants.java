b668729c5965522d059f5913c7c501d4e0ebb048
BenBernardCIS <benbernard@post.harvard.edu>
2020-02-09 21:49:21 -0500

    Merge branch 'master' into Spinner/#17-Count-Segments
    # Conflicts:
    #	src/main/java/org/frc5687/infiniterecharge/robot/Constants.java

--- src/main/java/org/frc5687/infiniterecharge/robot/Constants.java
+++ src/main/java/org/frc5687/infiniterecharge/robot/Constants.java
@@ -167,15 +167,11 @@ public class Constants {
     public class Spinner {
         public static final double MOTOR_PERCENT_SPEED = 0.5; // TODO: Need a real value here!
         public static final double COLOR_TOLERANCE = 0.06;
-<<<<<<< HEAD
-        public static final double PRE_INDEXER_SPEED = 1.0;
+        public static final double PRE_INDEXER_SPEED = -1.0;
         public static final double MOTOR_SLOW_PERCENT_SPEED = 0.2; // TODO: Need a real value here!
         public static final int AUTOSPIN_SLOW_AT_WEDGES = 20;
         public static final int AUTOSPIN_STOP_AT_WEDGES = 25;
         public static final double SENSOR_SAMPLE_PERIOD_SECONDS = 0.01;
-=======
-        public static final double PRE_INDEXER_SPEED = -1.0;
->>>>>>> master
     }
 
     public class RotarySwitch {
