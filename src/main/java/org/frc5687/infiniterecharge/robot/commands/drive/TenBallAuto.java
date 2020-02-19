514d2f09bc923c0dcc453d6b971c052b457aa68e
Dennis_Slobodzian <dennis.slobodzian@baxter-academy.org>
2020-02-18 19:11:36 -0500

    Ten Ball changed

--- src/main/java/org/frc5687/infiniterecharge/robot/commands/drive/TenBallAuto.java
+++ src/main/java/org/frc5687/infiniterecharge/robot/commands/drive/TenBallAuto.java
@@ -5,31 +5,38 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import org.frc5687.infiniterecharge.robot.commands.AutoAlign;
-import org.frc5687.infiniterecharge.robot.commands.AutoDrivePath;
-import org.frc5687.infiniterecharge.robot.commands.AutoIntake;
+import org.frc5687.infiniterecharge.robot.commands.*;
 import org.frc5687.infiniterecharge.robot.subsystems.*;
+import org.frc5687.infiniterecharge.robot.util.Limelight;
+import org.frc5687.infiniterecharge.robot.util.PoseTracker;
 
 public class TenBallAuto extends SequentialCommandGroup {
-    public TenBallAuto(DriveTrain driveTrain, Turret turret, Shooter shooter, Hood hood, Intake intake, AHRS imu) {
+    public TenBallAuto(DriveTrain driveTrain, Turret turret, Shooter shooter, Hood hood, Intake intake, Indexer indexer, AHRS imu, Limelight limelight, PoseTracker poseTracker, Lights lights) {
         addCommands(
-//                new ParallelDeadlineGroup(
-//                        new AutoDrivePath(driveTrain, imu, "Snipe", 0,false),
-//                        new AutoIntake(intake)
-//                )
-////                ,new WaitCommand(1)
-////                , new AutoAlign(driveTrain, 50)
-//              ,  new AutoDrivePath(driveTrain, imu, "SnipeToShoot", 0, true)
-////              ,  new AutoAlign(driveTrain, 0)
-//              ,  new ParallelDeadlineGroup(
-//                        new AutoDrivePath(driveTrain, imu, "ShootToGenerator", 0, false),
-//                        new AutoIntake(intake)
-//                )
-//              ,  new AutoAlign(driveTrain, -100)
-//              ,  new ParallelDeadlineGroup(
-//                        new AutoDrivePath(driveTrain, imu, "BallsToTrench", 0, false),
-//                        new AutoIntake(intake)
-//                )
+                new ParallelDeadlineGroup(
+                        new AutoDrivePath(driveTrain, imu, "Snipe", 0,false),
+                        new AutoIntake(intake, lights)
+                )
+              ,  new AutoDrivePath(driveTrain, imu, "SnipeToShoot", 0, true)
+                ,new ParallelDeadlineGroup(
+                        new AutoShoot(shooter, indexer, turret, null)
+                        ,new AutoTarget(turret, shooter, hood, limelight, driveTrain, poseTracker, lights,null,4300, 61) //TODO: Tune
+                )
+              ,  new ParallelDeadlineGroup(
+                        new AutoDrivePath(driveTrain, imu, "ShootToGenerator", 0, false),
+                        new AutoIntake(intake, lights)
+                )
+                ,new AutoDrivePath(driveTrain, imu, "HalfTrench", 0, false)
+
+                ,  new ParallelDeadlineGroup(
+                        new AutoDrivePath(driveTrain, imu, "TrenchBalls", 0, false),
+                        new AutoIntake(intake, lights)
+                )
+                ,new ParallelDeadlineGroup(
+                        new AutoShoot(shooter, indexer, turret, null),
+                        new AutoAlign(driveTrain, 0),
+                        new AutoTarget(turret, shooter, hood, limelight, driveTrain, poseTracker, lights,null,4500, 63) //TODO: Tune
+                )
         );
     }
 
