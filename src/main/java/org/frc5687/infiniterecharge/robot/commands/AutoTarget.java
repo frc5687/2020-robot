c6d8af427bb1dca1deabc489802bce8e45e599f3
BenBernardCIS <benbernard@post.harvard.edu>
2020-02-22 23:07:28 -0500

    no message

--- src/main/java/org/frc5687/infiniterecharge/robot/commands/AutoTarget.java
+++ src/main/java/org/frc5687/infiniterecharge/robot/commands/AutoTarget.java
@@ -1,6 +1,8 @@
 package org.frc5687.infiniterecharge.robot.commands;
 
 import edu.wpi.first.wpilibj.MedianFilter;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.frc5687.infiniterecharge.robot.Constants;
 import org.frc5687.infiniterecharge.robot.OI;
 import org.frc5687.infiniterecharge.robot.RobotPose;
@@ -21,6 +23,8 @@ public class AutoTarget extends OutliersCommand {
     private double _speed;
     private double _angle;
     private OI _oi;
+
+    private Mode _mode;
 
     public AutoTarget(Turret turret,
                       Shooter shooter,
@@ -56,18 +60,28 @@ public class AutoTarget extends OutliersCommand {
         _shooter.setVelocitySpeed(_speed);
 
         _lights.setTargeting(true);
+        _mode = Mode.Rough;
     }
 
     @Override
     public void execute() {
-        if (!_shooter.isShooting()) {
-            _turret.setMotionMagicSetpoint(_limelight.getHorizontalAngle() + _turret.getPositionDegrees());
+        switch (_mode) {
+            case Rough:
+                _turret.setMotionMagicSetpoint(_driveTrain.getAngleToTarget());
+                if (_turret.isAtSetpoint()) {
+                    _mode = Mode.Limelighting;
+                }
+                break;
+            case Limelighting:
+                if (!_shooter.isShooting()) {
+                    _turret.setMotionMagicSetpoint(_limelight.getHorizontalAngle() + _turret.getPositionDegrees());
+                }
+                if (!_shooter.isShooting()) {
+                    _turret.setMotionMagicSetpoint(_limelight.getHorizontalAngle() + _turret.getPositionDegrees() + _turret.getManualOffset());
+                }
+                _lights.setReadyToshoot(_shooter.isAtTargetVelocity() && _turret.isTargetInTolerance());
+                break;
         }
-        if (!_shooter.isShooting()) {
-            _turret.setMotionMagicSetpoint(_limelight.getHorizontalAngle() + _turret.getPositionDegrees() + _turret.getManualOffset());
-        }
-        _lights.setReadyToshoot(_shooter.isAtTargetVelocity() && _turret.isTargetInTolerance());
-
     }
 
 
@@ -96,5 +110,16 @@ public class AutoTarget extends OutliersCommand {
         _lights.setReadyToshoot(false);
         _hood.setPosition(Constants.Hood.MIN_DEGREES);
         _limelight.disableLEDs();
+        Command hoodCommand = _hood.getDefaultCommand();
+        if (hoodCommand instanceof DriveHood) {
+            ((DriveHood)hoodCommand).setZeroing(true);
+        }
+
     }
+
+    private enum Mode {
+        Rough,
+        Limelighting
+    }
+
 }
